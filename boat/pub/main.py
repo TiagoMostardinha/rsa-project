e2b4c1087532d485b48c2b746c1c33e63ac9bc78
Pinto1088 <lucaspinto042@gmail.com>
2024-05-25 22:06:45 +0100

    main

--- boat/pub/main.py
+++ boat/pub/main.py
@@ -4,12 +4,14 @@ from models.Boat import Boat
 import time
 
 def main():
-    batman = Batman()
+    batman = Batman()  # Create an instance of Batman
     boat = Boat("1", "idle", 1, 0, Location('0', 0, 0), [], Location('0', 0, 0), [])
 
     while True:
-        boat.neighbours = batman.get_neighbours()
+        boat.neighbours = batman.get_devices_map()  # Call with the instance
         print(boat.toJSON())
         time.sleep(1)
-        
-        
+
+
+if __name__ == "__main__":
+    main()
